38fd6a8bdc2693d2221e653ca07b9a1dc2ccdee5
r4z4 <r4z4aa@gmail.com>
2023-05-06 05:45:59 +0000

    card component

--- fan_can/lib/fan_can_web/components/core_components.ex
+++ fan_can/lib/fan_can_web/components/core_components.ex
@@ -656,4 +656,29 @@ defmodule FanCanWeb.CoreComponents do
   def translate_errors(errors, field) when is_list(errors) do
     for {^field, {msg, opts}} <- errors, do: translate_error({msg, opts})
   end
+
+  @doc """
+  Custom Component for State Information Cards
+  """
+  def cards(assigns) do
+    ~H"""
+    <%= for item <- @items do %>
+      <div class="card review-card">
+        <div class="card-header">
+          <%= for _ <- 0..item.ranking do %>
+            <i class="bi bi-star-fill"></i>
+          <% end %>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">
+            <strong>by: <%= item.title %></strong>
+          </h5>
+          <p class="card-text">
+            <%= item.notes %>
+          </p>
+        </div>
+      </div>
+    <% end %>
+    """
+  end
 end
