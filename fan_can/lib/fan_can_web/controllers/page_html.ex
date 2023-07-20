18aae2f8c29058ff340b605f6da79dc7e9269a82
r4z4 <r4z4aa@gmail.com>
2023-07-20 02:29:38 +0000

    state dropdown

--- fan_can/lib/fan_can_web/controllers/page_html.ex
+++ fan_can/lib/fan_can_web/controllers/page_html.ex
@@ -1,6 +1,11 @@
 defmodule FanCanWeb.PageHTML do
   use FanCanWeb, :html
   alias Heroicons.LiveView
+  alias FanCan.Core.Utils
 
   embed_templates "page_html/*"
+
+  def map_zip do
+    Enum.zip(Utils.states, Utils.state_names ++ Utils.territories)
+  end
 end
